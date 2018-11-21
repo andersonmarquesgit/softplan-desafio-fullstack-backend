57396412609c0ebf9ec75ad0bffc513a4afa7596
Anderson Marques <andersonmarques.ci@gmail.com>
2018-11-21 16:19:04 -0300

    Implementação do UserService

--- src/main/java/com/softplan/restapi/api/service/impl/UserServiceImpl.java
+++ src/main/java/com/softplan/restapi/api/service/impl/UserServiceImpl.java
@@ -1,42 +1,43 @@
 package com.softplan.restapi.api.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import com.softplan.restapi.api.entity.User;
+import com.softplan.restapi.api.repository.UserRepository;
 import com.softplan.restapi.api.service.UserService;
 
 @Service
 public class UserServiceImpl implements UserService {
 
+	@Autowired
+	private UserRepository userRepository;
+	
 	@Override
 	public User findByEmail(String email) {
-		// TODO Auto-generated method stub
-		return null;
+		return this.userRepository.findByEmail(email);
 	}
 
 	@Override
 	public User createOrUpdate(User user) {
-		// TODO Auto-generated method stub
-		return null;
+		return this.userRepository.save(user);
 	}
 
 	@Override
 	public User findById(String id) {
-		// TODO Auto-generated method stub
-		return null;
+		return this.userRepository.findOneById(id);
 	}
 
 	@Override
 	public void delete(String id) {
-		// TODO Auto-generated method stub
-
+		this.userRepository.deleteById(id);
 	}
 
 	@Override
 	public Page<User> findAll(int page, int count) {
-		// TODO Auto-generated method stub
-		return null;
+		return this.userRepository.findAll(PageRequest.of(page, count));
 	}
 
 }
